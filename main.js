056b396db8de88ca6d7d0504dd9db02755f7fbdc
Andrade1204 <proyectoclase2023@gmail.com>
2026-04-26 14:27:02 -0700

    Mostrando el nombre

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const a = 'hola';
 const num1 = 14;
 const rama = 'estamos en la rama login';
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', () =>{
+    const name = document.getElementById("name");
+    name.textContent = "Henry";
+})
